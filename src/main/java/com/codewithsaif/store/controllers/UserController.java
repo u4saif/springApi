aa9b7a70c3d8b173b1188a1c4440941ab77367ae
shaikh Saifuddin <u4saif@gmail.com>
2025-09-11 19:56:13 +0530

    Updating existing User

--- src/main/java/com/codewithsaif/store/controllers/UserController.java
+++ src/main/java/com/codewithsaif/store/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.codewithsaif.store.controllers;
 
 import com.codewithsaif.store.dtos.RegisterUserRequest;
+import com.codewithsaif.store.dtos.UpdateUserRequest;
 import com.codewithsaif.store.dtos.UserDto;
 import com.codewithsaif.store.mappers.UserMapper;
 import com.codewithsaif.store.repositories.UserRepository;
@@ -40,6 +41,19 @@ public class UserController {
         if(user == null)
             return ResponseEntity.notFound().build();
         return ResponseEntity.ok(userMapper.toDto(user));
+    }
+
+    @PutMapping("/{id}")
+    private ResponseEntity<UserDto> updateUser(@PathVariable(name = "id") Long id,
+                                               @RequestBody UpdateUserRequest request){
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null)
+            return ResponseEntity.notFound().build();
+         userMapper.toUpdate(request,user);
+         userRepository.save(user);
+
+       return ResponseEntity.ok(userMapper.toDto(user));
+
     }
 
 }
--- src/main/java/com/codewithsaif/store/mappers/UserMapper.java
+++ src/main/java/com/codewithsaif/store/mappers/UserMapper.java
@@ -1,12 +1,15 @@
 package com.codewithsaif.store.mappers;
 
 import com.codewithsaif.store.dtos.RegisterUserRequest;
+import com.codewithsaif.store.dtos.UpdateUserRequest;
 import com.codewithsaif.store.dtos.UserDto;
 import com.codewithsaif.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
      UserDto toDto(User user);
      User toEntity(RegisterUserRequest user);
+     User toUpdate(UpdateUserRequest request, @MappingTarget User user);
 }
