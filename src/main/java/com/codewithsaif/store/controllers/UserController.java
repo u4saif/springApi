de9a45c2d139e0745cbb18c3ba2ae46030ee1a9c
shaikh Saifuddin <u4saif@gmail.com>
2025-09-13 20:01:14 +0530

    Business Validation with database check

--- src/main/java/com/codewithsaif/store/controllers/UserController.java
+++ src/main/java/com/codewithsaif/store/controllers/UserController.java
@@ -30,9 +30,14 @@ public class UserController {
     }
 
     @PostMapping
-    private ResponseEntity<UserDto> registerUser(
+    private ResponseEntity<?> registerUser(
             @Valid  @RequestBody RegisterUserRequest request,
             UriComponentsBuilder uriBuilder){
+        if(userRepository.existsByEmail(request.getEmail())) {
+            return ResponseEntity.badRequest().body(
+                    Map.of("message","Email already registered"\
+            );
+        };
         var user = userMapper.toEntity(request);
         userRepository.save(user);
         var userDto = userMapper.toDto(user);
--- src/main/java/com/codewithsaif/store/repositories/UserRepository.java
+++ src/main/java/com/codewithsaif/store/repositories/UserRepository.java
@@ -2,7 +2,10 @@ package com.codewithsaif.store.repositories;
 
 import com.codewithsaif.store.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+//    @Query("select (count(u) > 0) from User u where u.email = ?1")
+    boolean existsByEmail(String email);
 }
