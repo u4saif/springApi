de8bd82a0a8daa6d05a9d8b3cdc06e52ca869c2a
shaikh Saifuddin <u4saif@gmail.com>
2025-09-23 22:03:05 +0530

    Added Security starter package

---
+++ src/main/java/com/codewithsaif/store/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.codewithsaif.store.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        //stateless session token based
+        //Diable csrf
+        //Authorize
+        http
+                .sessionManagement( c->
+                        c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(c-> c
+                        .requestMatchers("/carts/**").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/users").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
